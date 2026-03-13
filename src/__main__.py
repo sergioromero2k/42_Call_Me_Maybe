593ec54d337a6f0b9f77436e93c4b67e3228d8d7
Sergio Alejandro <sergioromero2k@gmail.com>
2026-03-13 16:18:58 +0100

    day 01 finished, adding main.py, utils.py

---
+++ src/__main__.py
@@ -0,0 +1,49 @@
+import argparse
+import json
+import sys
+from pathlib import Path
+from pydantic import ValidationError
+from src.utils import load_function_definitions, load_function_tests
+
+
+def main() -> None:
+    """
+    Load function definitions and test cases from JSON files,
+    validate them, and print the numeber of loaded items.
+    """
+    parser = argparse.ArgumentParser(
+        description="42 Call Me Maybe - LLM Function Caller"
+    )
+    # Command-line arguments for input and output directions
+    parser.add_argument("--input", default="data/input", type=str)
+    parser.add_argument("--output", default="data/output", type=str)
+    args = parser.parse_args()
+
+    input_path = Path(args.input)
+    route_definitions = input_path + "/functions_definition.json"
+    route_tests = input_path + "/function_calling_tests.json"
+
+    try:
+        # Load and validate function definitions and tests
+        functions = load_function_definitions(route_definitions)
+        tests = load_function_tests(route_tests)
+        print(
+            f"Success: Loaded {len(functions)} "
+            f"functions and {len(tests)} tests.")
+
+    except FileNotFoundError as e:
+        print(f"Error: File not found - {e.filename}")
+        sys.exit(1)
+    except json.JSONDecodeError as e:
+        print(f"Error: Invalid JSON format - {e.msg}")
+        sys.exit(1)
+    except ValidationError as e:
+        print(f"Error: Data validation failed - {e.json()}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from re import sub
+
+
+def fn_add_numbers(a: int, b: int) -> int:
+    """Return the sum of two integers."""
+    return a + b
+
+
+def fn_greet(name: str) -> str:
+    """Return a greeting message fot the given name."""
+    return f"Hi {name}, How are you?"
+
+
+def fn_reverse_string(s: str) -> str:
+    """REturn the reversed version of the input string."""
+    return s[::-1]
+
+
+def fn_get_square_root(a: int) -> int:
+    """Return the square root of a number."""
+    return a**(0.5)
+
+
+def fn_substitute_string_with_regex(
+        source_string: str, regex: str, replacement: str) -> str:
+    """Replace matches of a regex pattern in a string."""
+    return sub(regex, replacement, source_string)
+
+
+def load_function_definitions():
+    ...
+
+
+def load_function_tests():
+    ...
+
+
+# def fn_substitute_string_with_regex(
+#         source_string: str, regex: str, replacement: str) -> str:
+#     for i in source_string:
+#         if i == regex:
+#             source_string[i] = regex
+#     return source_string
