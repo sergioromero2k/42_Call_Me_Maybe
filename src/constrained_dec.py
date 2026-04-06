16520cb363a71bb162a774600e9f93332059bd38
Sergio Alejandro <sergioromero2k@gmail.com>
2026-04-06 15:09:38 +0200

    finished constrained

--- src/constrained_dec.py
+++ src/constrained_dec.py
@@ -2,6 +2,7 @@
 
 import json
 from llm_sdk.llm_sdk import Small_LLM_Model
+from src.models import FunctionDefinition
 
 
 class VocabularyMapper:
@@ -21,3 +22,43 @@ class VocabularyMapper:
             id for id, data in self.vocab_inverted.items()
             if data.startswith(prefix)
         ]
+
+
+class FunctionTrie:
+    def __init__(self):
+        self.root = {"children": {}, "is_end": False, "fn_name": None}
+
+    def insert(self, tokens, fn_name):
+        current_node = self.root
+
+        for token in tokens:
+            if token in current_node["children"]:
+                current_node = current_node["children"][token]
+            else:
+                new_node = {"children": {}, "is_end": False, "fn_name": None}
+                current_node["children"][token] = new_node
+                current_node = new_node
+
+        current_node["is_end"] = True
+        current_node["fn_name"] = fn_name
+
+    def get_valid_tokens(self, token_generated):
+        current_node = self.root
+
+        for token in token_generated:
+            if token in current_node["children"]:
+                current_node = current_node["children"][token]
+            else:
+                return []
+        return list(current_node["children"].keys())
+
+
+def build_trie(
+    functions: list[FunctionDefinition], model: Small_LLM_Model
+) -> FunctionTrie:
+    trie = FunctionTrie()
+
+    for function in functions:
+        tokens = model.encode(function.name).tolist()[0]
+        trie.insert(tokens, function.name)
+    return trie
